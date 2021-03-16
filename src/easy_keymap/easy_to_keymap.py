25d383fe90c6099055f501bf0134471994de11ad
Thom Smith <thom@thomthesmith.com>
2021-03-16 15:59:56 -0400

    More organization.

--- src/easy_keymap/easy_to_keymap.py
+++ src/easy_keymap/easy_to_keymap.py
@@ -17,57 +17,62 @@ CONDITION_EXPR = re.compile(r"""
 """, re.X)
 
 
-def easy_to_keymap(easy):
-    def get_condition(condition):
-        if isinstance(condition, str):
-            match = CONDITION_EXPR.match(condition)
+def get_condition(condition):
+    if isinstance(condition, str):
+        match = CONDITION_EXPR.match(condition)
 
-            ret = {
-                'key': match.group('key'),
-                'operator': match.group('operator'),
-                'operand': ruamel.yaml.safe_load(match.group('operand')),
-            }
+        ret = {
+            'key': match.group('key'),
+            'operator': match.group('operator'),
+            'operand': ruamel.yaml.safe_load(match.group('operand')),
+        }
 
-            if match.group('quantifier'):
-                ret['match_all'] = True
-
-            return ret
-        else:
-            return condition
-
-    def get_command(command):
-        if isinstance(command, dict):
-            commands = list(command.items())
-
-            if len(commands) == 1:
-                name, args = commands[0]
-                return ( name, args )
-        else:
-            return ( command, None )
-
-    def get_entry(entry):
-        ret = entry.copy()
-
-        if isinstance(ret['keys'], str):
-            ret['keys'] = [ ret['keys'] ]
-
-        if isinstance(ret['command'], list):
-            ret['args'] = {
-                'commands': [
-                    [ command, args ]
-                    for command, args in map(get_command, ret['command'])
-                ]
-            }
-            ret['command'] = 'chain'
-        else:
-            command, args = get_command(ret['command'])
-            ret['command'] = command
-            if args is not None:
-                ret['args'] = args
-
-        if 'context' in ret:
-            ret['context'] = list(map(get_condition, ret['context']))
+        if match.group('quantifier'):
+            ret['match_all'] = True
 
         return ret
+    else:
+        return condition
 
+
+def get_command(command):
+    if isinstance(command, dict):
+        commands = list(command.items())
+
+        if len(commands) == 1:
+            name, args = commands[0]
+            return ( name, args )
+    else:
+        return ( command, None )
+
+
+def get_entry(entry):
+    ret = {}
+
+    if isinstance(entry['keys'], str):
+        ret['keys'] = [ entry['keys'] ]
+    else:
+        ret['keys'] = entry['keys']
+
+    if isinstance(entry['command'], list):
+        ret['command'] = 'chain'
+        ret['args'] = {
+            'commands': [
+                [ command, args ]
+                for command, args in map(get_command, entry['command'])
+            ]
+        }
+    else:
+        command, args = get_command(entry['command'])
+        ret['command'] = command
+        if args is not None:
+            ret['args'] = args
+
+    if 'context' in entry:
+        ret['context'] = list(map(get_condition, entry['context']))
+
+    return ret
+
+
+def easy_to_keymap(easy):
     return list(map(get_entry, easy))
