d9dd6b611f6a5ae604e89edd6b809928affc81d7
Thomas Smith <thomas.smith@reifyhealth.com>
2022-02-17 13:46:06 -0500

    Fix bug in indentation chanmge listener

--- src/commands/tab_size_listener.py
+++ src/commands/tab_size_listener.py
@@ -31,6 +31,8 @@ class ResizeExistingTabsCommand(sublime_plugin.TextCommand):
 
 
 class TabSizeListener(ViewSettingsListener):
+    _loaded = False
+
     @classmethod
     def is_applicable(cls, settings: sublime.Settings) -> bool:
         return settings.get('translate_tabs_to_spaces', False)
@@ -39,8 +41,13 @@ class TabSizeListener(ViewSettingsListener):
     def applies_to_primary_view_only(cls) -> bool:
         return True
 
+    def on_load(self) -> None:
+        self._loaded = True
+
     @on_setting_changed('tab_size')
     def tab_size_changed(self, new_value: object, old_value: object) -> None:
+        if not self._loaded:
+            return
         self.view.run_command('resize_existing_tabs', {
             'new_size': new_value,
             'old_size': old_value,
